feb427c86e6c129747d6c3f6453da40057f5df0e
Colin Brayton <colin.brayton17@gmail.com>
2021-02-16 17:14:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# nuxt-demo
+
+> Nuxt.js starter using Semantic UI
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install
+
+# build Semantic UI assets
+$ npm run semantic:build
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+```
+
+More info: https://nuxtjs.org
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>nutxjs + Fomantic-UI!</h1>
+    </div>
+    <div>
+      <sui-button primary>Primary</sui-button>
+      <sui-button secondary>Secondary</sui-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
